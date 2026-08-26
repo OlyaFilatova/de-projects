3093e8626471b1a6ef076e05bdb26ab71634bc12
Olha Filatova <olyafilatov@gmail.com>
2026-08-26 03:34:49 +0300

    Update 01_hello_world.py

--- 000-bootcamp/homeworks/02/flows/module_redo_airflow/dags/01_hello_world.py
+++ 000-bootcamp/homeworks/02/flows/module_redo_airflow/dags/01_hello_world.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from airflow.sdk import dag, get_current_context, task, Param
 
 @dag(
@@ -18,19 +16,41 @@ from airflow.sdk import dag, get_current_context, task, Param
 )
 def hello_world():
     @task
-    def hello_message(name: str):
+    def hello_message():
       """
       #### Say hi.
       """
       context = get_current_context()
       name = context["params"]["name"]
-
       output = f"Hello, {name}!"
       print(output)
       return output
 
-    name = "{{ params.name }}"
+    @task
+    def generate_output():
+        return "I was generated during this Hello World workflow"
 
-    hello_message(name)
+    @task
+    def sleep():
+        import time
+        time.sleep(15)
+
+    @task
+    def log_output(output: str):
+        print(f"This is an output: {output}")
+
+    @task
+    def goodbye_message():
+      context = get_current_context()
+      name = context["params"]["name"]
+      print(f"Goodbye, {name}!")
+
+    hello = hello_message()
+    output = generate_output()
+    wait = sleep()
+    logged_output = log_output(output)
+    goodbye = goodbye_message()
+
+    hello >> output >> wait >> logged_output >> goodbye # type: ignore
 
 hello_world()
